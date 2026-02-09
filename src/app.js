382a3f36683b57d99478f1925c4ffe40393837cd
saisudhanv <saisudhanv2004@gmail.com>
2026-02-09 16:58:49 +0530

    Working on authentication

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
@@ -19,5 +20,17 @@ app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello Acquisitions!');
 });
+
+
+app.get('/health', (req,res) => {
+  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
+});
+
+app.get('/api', (req,res) => {
+  res.status(200).json({ message:'Acquisitions API is running'});
+});
+
+app.use('/api/auth', authRoutes);
+
 
 export default app;
