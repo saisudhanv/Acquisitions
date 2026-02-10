63e3ee44a900cdb1486a6b7a582898a050ef59fe
saisudhanv <saisudhanv2004@gmail.com>
2026-02-10 16:49:56 +0530

    Authentication completed

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,51 @@
+import logger from "#config/logger.js";
+import { signupSchema } from "#validations/auth.validation.js";
+import { formatValidationError } from "#utils/format.js";
+import { createUser } from "#services/auth.service.js";
+import { jwttoken } from "#utils/jwt.js";
+import { cookies } from "#utils/cookies.js";
+
+export const signup = async (req, res, next) => {
+    try {
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).json({
+                error: 'Validation error',
+                details: 'Request body is required'
+            }); 
+        }
+
+        const validationResult = signupSchema.safeParse(req.body);
+        if(!validationResult.success)
+        {
+            return res.status(400).json({
+                error: 'Validation error',
+                details: formatValidationError(validationResult.error)
+            });
+        }
+
+        const {name,email,password,role} = validationResult.data;
+
+        const user = await createUser({name,email,password,role});
+
+        const token = jwttoken.sign({id: user.id, email: user.email, role: user.role});
+        cookies.set(res,'token', token);
+
+        logger.info(`User registered successfully, ${email}`);
+        res.status(201).json({
+            message: 'User registered',
+            user: {
+                id:user.id, name:user.name, email:user.email, role:user.role
+            }
+        });
+
+    } catch (e)
+    {
+        logger.error('Signup error',e);
+        if (e.message === 'User with this email already exists')
+        {
+            return res.status(400).json({error: 'Email already exists'});
+        }
+        next(e);
+    }
+
+};
